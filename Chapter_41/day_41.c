af95aeacff10e2df0e34f68b79de88d63f631ad5
rakou.x1_windows <rakougit@gmail.com>
2023-06-29 13:22:50 +0900

    day_39

---
+++ Chapter_41/day_41.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    enum Company
+    {
+        GOOGLE,
+        FACEBOOK,
+        XEROX,
+        YAHOO,
+        EBAY,
+        MICROSOFT
+    };
+
+    enum Company xerox = XEROX;
+    enum Company google = GOOGLE;
+    enum Company ebay = EBAY;
+
+    printf("%d\n", xerox);
+    printf("%d\n", google);
+    printf("%d\n", ebay);
+
+    return 0;
+}
